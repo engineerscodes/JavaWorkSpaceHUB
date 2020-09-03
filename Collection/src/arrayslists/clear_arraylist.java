b02c3a08fdde20e3a4052863d8ec0645bdbc4b68
engineerscodes <practicalengineers05@gmail.com>
2020-09-03 21:09:37 +0530

    updated commit

--- Collection/src/arrayslists/clear_arraylist.java
+++ Collection/src/arrayslists/clear_arraylist.java
@@ -18,7 +18,7 @@ public class clear_arraylist  extends arraylist
 	public void clears()
 	{
 		d.clear();
-		//d.removeAll(d); //remove array's..............................using remveAll()
+		//d.removeAll(d); //remove array's.................using remveAll()
 		System.out.println("AFTER CLEARING DISPLAYING THE ARRAYLIST -");
 		System.out.println(d);
 		System.out.println("Checking the above array list is empty or not! "+d.isEmpty());
