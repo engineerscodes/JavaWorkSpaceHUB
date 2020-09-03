ee463858b346072d3b258096eeded845d4a0c498
engineerscodes <practicalengineers05@gmail.com>
2020-09-03 21:41:50 +0530

    first commit

---
+++ Collection/src/arrayslists/increase_size_arlist.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package arrayslists;
+import java.util.*;
+/**
+ * @author M.NAVEEN
+   RANDOM CODER'S
+ *
+ */
+public class increase_size_arlist extends arraylist
+{
+   
+	public increase_size_arlist() 
+	{
+		super();
+	}
+	public static void main(String ...naveen)
+	{
+		new increase_size_arlist().str.ensureCapacity(100); //increases the capacity to 100 
+		
+	}
+
+}
