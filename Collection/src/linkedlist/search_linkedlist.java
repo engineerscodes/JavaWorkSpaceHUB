9fb0bb1db7acd0267277ebf6665645b79466affe
engineerscodes <practicalengineers05@gmail.com>
2020-09-16 22:42:08 +0530

    Initial commit

---
+++ Collection/src/linkedlist/search_linkedlist.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package linkedlist;
+import java.util.*;
+/**
+ * @author M.NAVEEN
+   RANDOM CODER'S
+ *
+ */
+public class search_linkedlist 
+{   static linkedlist n;
+    static Scanner nav;
+	public search_linkedlist() 
+	{   super();
+		n=new linkedlist();
+		nav=new Scanner(System.in);
+	}
+
+	public static void main(String[] args) 
+	{
+		search_linkedlist sl=new search_linkedlist();
+		n.add();
+		System.out.println("the linked list is:"+n.d);
+		System.out.println("Enter the element of search ");
+	    if(n.d.contains(nav.nextInt()))
+	    	System.out.println("The element is present");
+	    else
+		System.out.println("The element is not present");
+	    
+	}
+
+}
