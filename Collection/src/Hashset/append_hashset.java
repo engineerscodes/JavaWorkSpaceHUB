669b8a01f6e692caf014214fbd08d47c54cb032b
engineerscodes <practicalengineers05@gmail.com>
2020-09-21 09:46:53 +0530

    Initial commit

---
+++ Collection/src/Hashset/append_hashset.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package Hashset;
+import java.util.*;
+/**
+ * @author M.NAVEEN
+   RANDOM CODER'S
+ *
+ */
+public class append_hashset 
+{
+   static  HashSet <Integer> h;
+
+	public append_hashset() 
+	{
+	h= new HashSet<>();
+	}
+
+	public static void main(String ... arg)
+	{
+		System.out.println("IMPLEMENTING HASHSET");
+		 new append_hashset();
+	     int a[]={1,2,3,1,4,5,5,7,8,9,1,5,6,0,9,0};
+	     System.out.println("Displaying the array -"+Arrays.toString(a));
+	     for(int i=0;i<a.length;i++)
+	     {
+	    	 h.add(a[i]);
+	     }
+	     System.out.println("Hash Set ->"+h);  //h.size<=a.length always .
+	}
+}
