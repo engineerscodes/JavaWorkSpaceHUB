9b5c759e66e722994c07e2eeda752755d74bbeb5
engineerscodes <practicalengineers05@gmail.com>
2020-09-22 10:44:01 +0530

    Updated

--- Collection/src/Hashset/append_hashset.java
+++ Collection/src/Hashset/append_hashset.java
@@ -18,10 +18,10 @@ public class append_hashset
 	}
 
 	public static void main(String ... arg)
-	{
+	{   //HashSet is faster but doesnot maintain any order
 		System.out.println("IMPLEMENTING HASHSET");
 		 new append_hashset();
-	     int a[]={1,2,3,1,4,5,5,7,8,9,1,5,6,0,9,0};
+	     int a[]={1,2,3,1,4,5,5,7,8,9,1,5,6,0,9,0,100,56};
 	     System.out.println("Displaying the array -"+Arrays.toString(a));
 	     for(int i=0;i<a.length;i++)
 	     {
--- Collection/src/TreeSet/package-info.java
+++ Collection/src/TreeSet/package-info.java
@@ -7,10 +7,12 @@
  *
  */
 package TreeSet;
-/*
+/*TreeSet maintains an object in sorted order 
  * HashSet allows null object. TreeSet doesn’t allow null Object and throw NullPointerException, 
  * Why, because TreeSet uses compareTo() method to compare keys and 
  * compareTo() will throw java.lang.NullPointerException.
  * If you want a sorted Set then it is better to add elements to HashSet 
  * and then convert it into TreeSet rather than creating a TreeSet and adding elements to it.
+ * url=https://stackoverflow.com/questions/1463284/hashset-vs-treeset
+ *
  */
