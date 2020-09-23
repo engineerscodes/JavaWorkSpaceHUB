2fc170f4057d43d27db4113126c5c50bab892275
engineerscodes <practicalengineers05@gmail.com>
2020-09-23 13:55:10 +0530

    Initial Commit

---
+++ Collection/src/PriorityQueue/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+/**
+ * @author M.NAVEEN
+   RANDOM CODER'S
+ *
+ */
+package PriorityQueue;
+//PriorityQueue doesn’t permit null.
+/*
+ * PriorityQueue are unbound queues.
+ * PriorityQueue(int initialCapacity): 
+ * Creates a PriorityQueue with the specified initial capacity that 
+ * orders its elements according to their natural ordering.
+ */
