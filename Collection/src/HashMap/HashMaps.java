ea0d5a31721be12066cd638027f4add2c5f511f8
engineerscodes <practicalengineers05@gmail.com>
2020-09-28 11:54:32 +0530

    Hash Map

---
+++ Collection/src/HashMap/HashMaps.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package HashMap;
+import java.util.*;
+/**
+ * @author M.NAVEEN
+   RANDOM CODER'S
+ *
+ */
+public class HashMaps 
+{   static Scanner nav;
+     HashMap<Integer,String> map;
+	public HashMaps() 
+	{
+		nav=new Scanner(System.in);
+		map= new HashMap(); 
+	}
+ 
+	public void insert_to_Map()
+	{
+		//map.entrySet(1,"NAVEEN");
+		map.put(1,"NAVEEN");
+		map.put(2,"bala");
+		map.put(1,"james");//over writes the naveen
+		map.put(0,"praveen");
+		map.put(105,"RDJ");
+	}
+	public static void main(String[] args) 
+	{
+		HashMaps obj =new HashMaps();
+		obj.insert_to_Map();
+         for(Map.Entry x:obj.map.entrySet())
+        	 System.out.println(x.getKey()+" "+x.getValue());
+	}
+
+}
