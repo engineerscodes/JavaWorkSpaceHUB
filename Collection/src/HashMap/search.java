dfd921ec12e18793d652263bb37b5fb57a97c1fb
engineerscodes <practicalengineers05@gmail.com>
2020-09-29 10:53:14 +0530

    Initial Commit

---
+++ Collection/src/HashMap/search.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package HashMap;
+import java.util.*;
+/**
+ * @author M.NAVEEN
+   RANDOM CODER'S
+ *
+ */
+public class search {
+   static HashMap<Integer,String> map;
+   static Scanner nav;
+	public search()
+	{   nav=new Scanner(System.in);
+        map=new HashMap<Integer,String>();
+	}
+
+	public static void main(String[] args) 
+	{  new  search();
+      map.put(1,"red");
+      map.put(2,"blue");
+      map.put(3,"yellow");
+      System.out.println("HashMap :"+map);
+      System.out.println("Enter to check if the Key is present :");
+      if(map.containsKey(nav.nextInt()))
+    	  System.out.println("Is present");
+      else
+    	  System.out.println("Not present"); 
+      System.out.println("Enter to check if the Value is present :");
+      if(map.containsValue(nav.nextInt()))
+      {
+    	  System.out.println("Is present"); 
+      }
+      else
+    	  System.out.println("Value Not present"); 
+	}
+
+}
