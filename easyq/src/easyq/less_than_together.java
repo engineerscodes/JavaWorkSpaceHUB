1c41684201c0461df88a43d7ac4fdff22c001455
engineerscodes <practicalengineers05@gmail.com>
2021-01-13 23:39:10 +0530

    Initial Commit

---
+++ easyq/src/easyq/less_than_together.java
@@ -0,0 +1,28 @@
+package easyq;
+import java.util.*;
+public class less_than_together 
+{  static Scanner nav=new Scanner(System.in);
+   static int array[],size,temp,count=0,gcount=0,val;
+	public less_than_together(int c) 
+	{
+		size=c;
+		array=new int[size];
+	}
+	
+	public static void swap()
+	{
+		for(int i=0;i<size;i++)
+		{
+			if(array[i]<=val)
+				count++;
+			else
+				gcount++;
+		}
+		for( int i=0;i<size/2;i++)
+		{
+		     	
+		}
+		
+	}
+
+}
