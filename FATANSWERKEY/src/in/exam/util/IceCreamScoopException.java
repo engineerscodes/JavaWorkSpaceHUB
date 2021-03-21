b0bac8ab6801a510ac467c1098bc0a2593c9579e
engineerscodes <practicalengineers05@gmail.com>
2021-03-22 00:23:42 +0530

    Initial Commit

---
+++ FATANSWERKEY/src/in/exam/util/IceCreamScoopException.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package in.exam.util;
+
+/**
+ *  @author M.NAVEEN
+ *  RANDOM CODER'S
+ *  Tech/Project Lead Android Club
+ */
+public class IceCreamScoopException extends Exception{
+
+	public String toString()
+	{
+		return "ONLY 3 SCOOPS ARE ALLOWED";
+	}
+}
+
