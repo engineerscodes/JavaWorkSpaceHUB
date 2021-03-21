4888a37e6a15b3a65af57a599c365204e8499895
engineerscodes <practicalengineers05@gmail.com>
2021-03-22 00:23:54 +0530

    Initial Commit

---
+++ FATANSWERKEY/src/in/exam/util/IceCreamFlavorException.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package in.exam.util;
+
+/**
+ *  @author M.NAVEEN
+ *  RANDOM CODER'S
+ *  Tech/Project Lead Android Club
+ */
+public class IceCreamFlavorException extends Exception {
+
+	public String toString()
+	{
+		return "INVALID ICE CREAM FLAVOR";
+	}
+
+}
+
